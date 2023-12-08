63c7130fde7202389ab7e9a55aec42963d1f1483
MateoCornejo <mateoh.cornejo@gmail.com>
2023-12-08 05:31:49 -0300

    method to decide check squaredDistance between two points -> to draw with efficiency

--- src/backend/model/Point.java
+++ src/backend/model/Point.java
@@ -17,6 +17,14 @@ public class Point {
         return y;
     }
 
+    // chequeo esta distancia para saber si dibujo o no una figura
+    // utilidad? -> ahorrar procesamiento
+    public double distanceSquaredTo(Point other) {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return dx * dx + dy * dy;
+    }
+
     @Override
     public String toString() {
         return String.format("{%.2f , %.2f}", x, y);
