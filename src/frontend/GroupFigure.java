f0baf0365a6a513d64cc7e207ed8ffcd97516a14
MateoCornejo <mateoh.cornejo@gmail.com>
2023-12-15 20:58:59 -0300

    GroupFigure now extends HashSet to avoid repetitions when grouping figures

--- src/frontend/GroupFigure.java
+++ src/frontend/GroupFigure.java
@@ -1,11 +1,11 @@
 package frontend;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 import frontend.paintFigures.PaintFigure;
 
-public class GroupFigure extends ArrayList<PaintFigure>{
+public class GroupFigure extends HashSet<PaintFigure>{
 
     public GroupFigure(Collection<? extends PaintFigure> figures){
         super(figures);
