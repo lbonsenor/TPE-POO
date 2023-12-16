9bd31e0971318dfa45dbd78ede69be8c3673ebb0
MateoCornejo <mateoh.cornejo@gmail.com>
2023-12-15 21:40:20 -0300

    Generic changes in canvasState, GroupFigure and PaintFigure

--- src/frontend/GroupFigure.java
+++ src/frontend/GroupFigure.java
@@ -3,9 +3,10 @@ package frontend;
 import java.util.Collection;
 import java.util.HashSet;
 
+import backend.model.Figure;
 import frontend.paintFigures.PaintFigure;
 
-public class GroupFigure extends HashSet<PaintFigure>{
+public class GroupFigure<F extends Figure> extends HashSet<Figure>{
 
     public GroupFigure(Collection<? extends PaintFigure> figures){
         super(figures);
